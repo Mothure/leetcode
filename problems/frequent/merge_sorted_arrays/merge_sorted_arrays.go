64067b86b0e1611242f2da2b88dcd8464a9c1a7a
Mothure <110694501+Mothure@users.noreply.github.com>
2025-05-21 22:54:05 +0300

    feat: merge sorted arrays

---
+++ problems/frequent/merge_sorted_arrays/merge_sorted_arrays.go
@@ -0,0 +1,23 @@
+package merge_sorted_arrays
+
+//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//Output: [1,2,2,3,5,6]
+//Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+//The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	k := m + n - 1
+	i := m - 1
+	j := n - 1
+
+	for j >= 0 {
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+}
